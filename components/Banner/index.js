1fea86d248606b74ff58afb4fb94a6637203e704
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-01-24 13:17:35 -0800

    styling banner

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -7,7 +7,7 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 width: 100vw;
-height: 350px;
+height: 40vh;
 background-color: #F2DEE9;
 `
 
@@ -16,9 +16,11 @@ export function Banner() {
         <>
             <BannerCont>
                 <Text
-                    text="Art Inspiration Generator" />
+                    text="Art Inspiration Generator"
+                    size="32px" />
                 <Text
-                    text="Lorem ipsum blah blah blah"
+                    text="Lorem ipsum dolor sit amet consectetur. Id non at adipiscing sed morbi vel sit urna lectus. Eget lacus morbi facilisis mattis enim. Nisi vehicula orci praesent morbi cras placerat diam. Magna egestas vitae imperdiet eget sem duis molestie."
+                    padding="2em 5em 0 5em"
                     size="16px" />
             </BannerCont>
         </>
