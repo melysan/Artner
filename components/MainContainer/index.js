80f5fdeecc69b6b0bcb6028940ed8c41eb779d9f
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-01-26 10:05:42 -0800

    add styling colourpalette and wordprompt comp

---
+++ components/MainContainer/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const MainContDiv = styled.div`
+display: flex;
+colour: #F4F5F0;
+padding: 2em;
+border`
+
+export function MainContainer() {
+
+    return (
+        <MainContDiv />
+    )
+}
--- pages/home.js
+++ pages/home.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import { Banner } from '../components/Banner'
+import { NavBar } from '../components/NavBar'
+import { WordPrompt } from '../components/WordPrompt'
+import { ColourPalette } from '../components/ColourPalette'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +21,13 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className={styles.main}>
-                <Banner></Banner>
+                <NavBar></NavBar>
+                <Banner
+                    bannerHeading="Art Inspiration Generator"
+                    bannerDescription="Lorem ipsum dolor sit amet consectetur. Id non at adipiscing sed morbi vel sit urna lectus. Eget lacus morbi facilisis mattis enim. Nisi vehicula orci praesent morbi cras placerat diam. Magna egestas vitae imperdiet eget sem duis molestie." />
+
+                <ColourPalette />
+                <WordPrompt />
                 This is the home page.
             </div>
 
