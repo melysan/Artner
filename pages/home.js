54083d88e3c02cef7bf37f1cd87e910cf615a69f
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-02-28 01:17:34 -0800

    home page styles

--- pages/home.js
+++ pages/home.js
@@ -6,7 +6,31 @@ import { WordPrompt } from '../components/WordPrompt'
 import { ColourPalette } from '../components/ColourPalette'
 import RandomImage from '../components/RandomImage/randomimage'
 import { Footer } from '../components/Footer'
+import styled from 'styled-components'
 
+const MainColumn = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 2rem;
+margin: 1rem;
+`
+
+const MainRow = styled.div`
+display: flex;
+flex-direction: row;
+align-items: flex-end;
+`
+
+const BoxContainer = styled.div`
+background-color: #F4F5F0;
+width: 60%;
+border: 1px black solid;
+padding: 2em 0 2em 0;
+border-radius: 10px;
+display: flex;
+justify-content: center;
+`
 
 export default function Home() {
 
@@ -18,15 +42,31 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className={styles.main}>
-                <NavBar />
-                <Banner
-                    bannerHeading="Art Inspiration Generator"
-                    bannerDescription="Sometimes getting inspiration for art is difficult, but that's why we created an art inspiration generator! Here, you can generate a random colour palette, a random six letter word prompt, or a random image! Or all three if you like. The possibilities are endless!" />
-                <ColourPalette />
-                <WordPrompt />
-                <RandomImage />
-            </div>
+            <NavBar />
+            <Banner
+                bannerHeading="Art Inspiration Generator"
+                bannerDescription="Sometimes getting inspiration for art is difficult, but that's why we created an art inspiration generator! Here, you can generate a random colour palette, a random six letter word prompt, or a random image! Or all three if you like. The possibilities are endless!" />
+
+            <MainColumn>
+                <BoxContainer>
+                    <MainColumn>
+
+                        <ColourPalette />
+                    </MainColumn>
+
+                </BoxContainer>
+                <BoxContainer>
+
+                    <MainRow>
+                        <MainColumn>
+
+                            <WordPrompt />
+                        </MainColumn>
+                        <RandomImage />
+                    </MainRow>
+                </BoxContainer>
+
+            </MainColumn>
 
             <Footer />
         </>
